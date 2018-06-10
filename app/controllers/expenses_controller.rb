1561e94413683384ef43b3f4c61e7c4ec2ac3d09
LuisTer15 <LuisTer15@github.com>
2018-06-10 18:47:01 -0500

    Fix some problems

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,17 +2,21 @@ class ExpensesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-     @expenses = current_user.expenses.order("date DESC")
+    if user_signed_in?
+      @expenses = current_user.expenses.order("date DESC")
 
-     def index
-       @expenses = current_user.expenses.order("date DESC")
+      def index
+        @expenses = current_user.expenses.order("date DESC")
 
-       if params[:concept].present?
-         @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
-       end
-       if params[:category_id].present?
-         @expenses = @expenses.where("category_id = ?", params[:category_id])
-       end
-     end
+        if params[:concept].present?
+          @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+        end
+        if params[:category_id].present?
+          @expenses = @expenses.where("category_id = ?", params[:category_id])
+        end
+      end
+    else
+      redirect_to sign_in_path
+    end
   end
 end
