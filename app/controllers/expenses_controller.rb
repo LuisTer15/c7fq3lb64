28fa8b4e82cf234967afe9f797542c9725647ebe
LuisTer15 <lternerah@gmail.com>
2018-06-12 13:18:08 -0500

    Update expenses_controller.rb

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,14 +1,17 @@
 class ExpensesController < ApplicationController
   before_action :authenticate_user!
 
-      def index
+    def index
+      if user_signed_in?
         @expenses = current_user.expenses.order("date DESC")
-
         if params[:concept].present?
           @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
         end
         if params[:category_id].present?
           @expenses = @expenses.where("category_id = ?", params[:category_id])
         end
+      else
+       redirect_to new_user_session_path
       end
+    end
 end
